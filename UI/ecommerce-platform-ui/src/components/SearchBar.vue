1002f5445ad9c1e573181476718a6cdbfcd57c7a
nhathuyit3 <hnhuy.17it3@sict.udn.vn>
2025-05-18 16:35:33 +0700

    Upload init project

---
+++ UI/ecommerce-platform-ui/src/components/SearchBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="search-bar-container">
+    <div class="icon-search"></div>
+    <div class="text-search">
+      <p>Search here...</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  setup() {
+    
+  },
+}
+</script>
+
+<style scoped>
+.search-bar-container {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: auto;
+}
+</style>
---
+++ UI/ecommerce-platform-ui/src/views/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="dashboard-container">
+    <search-bar />
+    <h3>Dashboard</h3>
+  </div>
+</template>
+
+<script>
+import SearchBar from '../components/SearchBar.vue'
+export default {
+  name: 'Dashboard',
+  components: { 
+    'search-bar': SearchBar,
+   },
+  setup() {
+    
+  },
+}
+</script>
+
+<style scoped>
+</style>
